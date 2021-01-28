bc0591f1ba9b452c62ace08c2af4439562a5552a
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2021-01-27 21:38:20 -0300

    Finalizando terceiro dia da Imersão React Next da Alura :rocket:

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,14 @@
 import { useState } from "react";
-import styled from "styled-components";
 import db from "../db.json";
 import Widget from "../src/components/Widget";
 import QuizLogo from "../src/components/QuizLogo";
 import QuizBackground from "../src/components/QuizBackground";
+import QuizContainer from "../src/components/QuizContainer";
 import Footer from "../src/components/Footer";
 import GitHubCorner from "../src/components/GitHubCorner";
+import Input from "../src/components/Input";
+import Button from "../src/components/Button";
 import { useRouter } from "next/router";
-
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
@@ -44,15 +28,16 @@ export default function Home() {
                 router.push(`/quiz?name=${name}`);
               }}
             >
-              <input
+              <Input
+                name="nomeDoUsuario"
                 type="text"
                 placeholder="Diz ai seu nome"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
-              <button type="submit" disabled={!name ? true : false}>
-                Jogar {name}
-              </button>
+              <Button type="submit" disabled={!name ? true : false}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>

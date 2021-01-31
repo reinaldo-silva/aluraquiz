828018dc0fda3409b5b0c1aeabf4a491b347a8cd
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2021-01-30 23:42:30 -0300

    Finalizando a Imersao React Next da Alura

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import db from "../db.json";
+import Head from "next/head";
+import { motion } from "framer-motion";
 import Widget from "../src/components/Widget";
 import QuizLogo from "../src/components/QuizLogo";
 import QuizBackground from "../src/components/QuizBackground";
 import QuizContainer from "../src/components/QuizContainer";
 import Footer from "../src/components/Footer";
+import Link from "../src/components/Link";
 import GitHubCorner from "../src/components/GitHubCorner";
 import Input from "../src/components/Input";
 import Button from "../src/components/Button";
@@ -15,9 +18,21 @@ export default function Home() {
   const [name, setName] = useState("");
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz -{db.title}</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: "0" },
+            hidden: { opacity: 0, y: "100%" },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
@@ -42,14 +57,49 @@ export default function Home() {
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: "0" },
+            hidden: { opacity: 0, y: "100%" },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1>Quizes da Galera</h1>
-
-            <p>lorem ipsum dolor sit amet...</p>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, "")
+                  .replace("https:", "")
+                  .replace(".vercel.app")
+                  .split(".");
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
-        <Footer />
+        <Footer
+          as={motion.footer}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: "0" },
+            hidden: { opacity: 0, y: "100%" },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/reinaldo-silva" />
     </QuizBackground>
--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import db from "../db.json";
-import Widget from "../src/components/Widget";
-import QuizLogo from "../src/components/QuizLogo";
-import QuizBackground from "../src/components/QuizBackground";
-import QuizContainer from "../src/components/QuizContainer";
-import AlternativesForm from "../src/components/AlternativesForms";
-import Button from "../src/components/Button";
+import db from "../../db.json";
+import Widget from "../../src/components/Widget";
+import QuizLogo from "../../src/components/QuizLogo";
+import QuizBackground from "../../src/components/QuizBackground";
+import QuizContainer from "../../src/components/QuizContainer";
+import AlternativesForm from "../../src/components/AlternativesForms";
+import Button from "../../src/components/Button";
 
 function ResultWidget({ results }) {
   return (
